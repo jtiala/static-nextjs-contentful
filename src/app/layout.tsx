0111d85d09cd97bbf47c06025ba655c42cfb27a1
Joonas Tiala <joonas.tiala@gmail.com>
2024-04-08 23:54:14 +0300

    Remove draft mode banner, which couldn't be used as I hoped

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
-import DraftModeBanner from "@/components/DraftModeBanner";
 import Heading from "@/components/Heading";
 import clsx from "clsx";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { draftMode } from "next/headers";
 import Link from "next/link";
 import "./globals.css";
 
@@ -27,7 +25,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={bodyClassName}>
-        {draftMode().isEnabled && <DraftModeBanner />}
         <header className="container mx-auto p-8 border-b border-neutral-700">
           <Heading level={1}>
             <Link href="/">⚽️ BallerBase</Link>
